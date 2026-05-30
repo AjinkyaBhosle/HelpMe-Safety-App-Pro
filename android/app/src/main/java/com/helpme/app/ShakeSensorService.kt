87d884d168cc00bdd11e171ed8fa36e68d748891
AjinkyaBhosle <ajinkyaab1991@gmail.com>
2026-05-30 23:25:17 +0530

    Increase shake threshold to 3.2G to make it require a genuinely firm shake

--- android/app/src/main/java/com/helpme/app/ShakeSensorService.kt
+++ android/app/src/main/java/com/helpme/app/ShakeSensorService.kt
@@ -28,7 +28,7 @@ class ShakeSensorService : Service(), SensorEventListener {
     private var wakeLock: PowerManager.WakeLock? = null
 
     // Shake detection parameters
-    private val SHAKE_THRESHOLD_GRAVITY = 2.5f
+    private val SHAKE_THRESHOLD_GRAVITY = 3.2f
     private val SHAKE_SLOP_TIME_MS = 500
     private val SHAKE_COUNT_RESET_TIME_MS = 3000
     private val SHAKE_MIN_COUNT = 2
